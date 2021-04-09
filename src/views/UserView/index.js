4a33192eba1ce4b7f37c1206d2310e47c1604552
Soslan <sparksoslan@gmail.com>
2021-04-09 12:42:37 +0100

    Add some small error handling to user details view

--- src/views/UserView/index.js
+++ src/views/UserView/index.js
@@ -4,6 +4,7 @@ import { useQuery } from 'react-query';
 import { fetchUser } from 'src/api/users';
 import { Box, Container, makeStyles, Typography } from '@material-ui/core';
 import UserCard from 'src/components/UserCard';
+import LoadingScreen from 'src/components/LoadingScreen';
 
 const useStyles = makeStyles(() => ({
   title: {
@@ -17,13 +18,33 @@ const UserView = () => {
 
   // Fetch the users from API. useQuery automatically caches the data.
   // Enables the use of Suspense fallback.
-  // Keep the data fresh in cache for 5 minutes or it doesn't refetch on each render
-  const { data: user } = useQuery(`user${userId}`, () => fetchUser(userId), {
-    refetchOnWindowFocus: false,
-    suspense: true,
-    staleTime: 60000 * 5,
-    retry: false,
-  });
+  // Keep the data fresh in cache for 5 minutes so it doesn't refetch on each render
+  const { data: user, isLoading, isError } = useQuery(
+    `user${userId}`,
+    () => fetchUser(userId),
+    {
+      refetchOnWindowFocus: false,
+      staleTime: 60000 * 5,
+      retry: false,
+    }
+  );
+
+  if (isLoading) return <LoadingScreen />;
+
+  if (isError) {
+    return (
+      <Typography
+        variant="h3"
+        component="h1"
+        align="center"
+        color="textPrimary"
+        id="events"
+        className={classes.title}
+      >
+        User does not exists!
+      </Typography>
+    );
+  }
 
   return (
     <Page title={`User ID: ${user.data.id} | Line-Up`}>
